c17d356fed32ac9dcbe04ded77ca75cffe6b1183
eestruve <6o6c@inbox.ru>
2022-01-30 19:10:21 +0300

    Домашнее задание 2

--- Задание 2.py
+++ Задание 2.py
@@ -1,15 +1,41 @@
 import requests
-offset_ = 2001
-limit_ = 1000
-url = f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/kn/object?offset={offset_}&limit={limit_}&sortField=devId.devShortCleanNm&sortType=asc&objStatus=0'
-res = requests.get(url)
-objects_data = res.json()
-list_id = objects_data.get('data').get('list')
-x = []
-for i in list_id:
-    x.append(i.get('objId'))
-print(type(x[1]))
+import pandas
+import time
+def downloading(offset, limit):
+    url = f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/kn/object?offset={offset}&limit={limit}&sortField=devId.devShortCleanNm&sortType=asc&objStatus=0'
+    res = requests.get(url)
+    objects_data = res.json()
+    list_id = objects_data.get('data').get('list')
+    return list_id
 
-url1 = f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/object/{x[1]}'
-res1 = requests.get(url1)
-objects_data1 = res1.json()
+offset_ = 1
+limit_ = 5048
+df = pandas.DataFrame()
+
+
+k = 0
+for j in range(2):
+    list_id = downloading(offset_,limit_)
+    for i in list_id:
+        if k == 5000:
+            objectID = i.get('objId')
+            url1 = f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/object/{objectID}'
+            res1 = requests.get(url1)
+            objects_data1 = res1.json()
+            objects_data1 = objects_data1.get('data')
+            df = df.append(pandas.json_normalize(objects_data1), ignore_index=True)
+            time.sleep(20)
+            k = 0
+        else:
+            print(k)
+            objectID = i.get('objId')
+            url1 = f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/object/{objectID}'
+            res1 = requests.get(url1)
+            objects_data1 = res1.json()
+            objects_data1 = objects_data1.get('data')
+            df = df.append(pandas.json_normalize(objects_data1), ignore_index=True)
+            k += 1
+    offset_ += 5048
+df.to_excel('Задание 2.xlsx',index=False)
+df.to_pickle('Задание 2.pickle')
+df.to_csv('Задание 2.csv')
